a2efc2d7ee4d2ce054268270255c109358e6a43a
BryanM <bryanmonroyleon7@gmail.com>
2025-02-20 23:57:31 -0600

    Tercer Commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 void determinarSigno();
-
+void verificarParImpar();
+void encontrarMayor();
 
 int main() {
     int opcion;
@@ -11,7 +12,7 @@ int main() {
     do {
         cout << "\nMENU PRINCIPAL" << endl;
         cout << "1. Estructura if-else" << endl;
-        cout << "0. Salir" << endl;
+        cout << "2. Salir" << endl;
         cout << "Seleccione una opcion: ";
         cin >> opcion;
 
@@ -21,7 +22,9 @@ int main() {
                 do {
                     cout << "\nSUBMENU - ESTRUCTURA IF-ELSE" << endl;
                     cout << "1. Determinar si un numero es positivo, negativo o cero" << endl;
-                    cout << "0. Volver al menu principal" << endl;
+                    cout << "2. Verificar si un numero es par o impar" << endl;
+                    cout << "3. Encontrar el mayor de tres numeros" << endl;
+                    cout << "4. Volver al menu principal" << endl;
                     cout << "Seleccione una subopcion: ";
                     cin >> subopcion;
 
@@ -29,13 +32,19 @@ int main() {
                         case 1:
                             determinarSigno();
                             break;
-                        case 0:
+                        case 2:
+                            verificarParImpar();
+                            break;
+                        case 3:
+                            encontrarMayor();
+                            break;
+                        case 4:
                             cout << "Volviendo al menu principal..." << endl;
                             break;
                         default:
                             cout << "Opcion invalida, intente nuevamente." << endl;
                     }
-                } while (subopcion != 0);
+                } while (subopcion != 4);
                 break;
             }
             case 2:
@@ -62,4 +71,27 @@ void determinarSigno() {
         cout << "El numero es cero." << endl;
 }
 
+void verificarParImpar() {
+    int numero;
+    cout << "Ingrese un numero: ";
+    cin >> numero;
 
+    if (numero % 2 == 0)
+        cout << "El numero es par." << endl;
+    else
+        cout << "El numero es impar." << endl;
+}
+
+void encontrarMayor() {
+    int num1, num2, num3;
+    cout << "Ingrese tres numeros: ";
+    cin >> num1 >> num2 >> num3;
+
+    int mayor = num1;
+    if (num2 > mayor)
+        mayor = num2;
+    if (num3 > mayor)
+        mayor = num3;
+
+    cout << "El mayor de los tres numeros es: " << mayor << endl;
+}
